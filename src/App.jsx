3d82c98d9bd65dad17daa0f1fe954909ae9cdff3
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-23 08:45:13 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/header/HeaderComponent';
+
+const App = () => {
+  return (
+    <div>
+      {/* <header /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/loginScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loginScreen = () => {
+  return (
+    <div>
+      <div className="logo-container">
+        
+      </div>
+    </div>
+  );
+};
+
+export default loginScreen;
